c8635df7916605c4d0a719e6c86295c6d717ad00
Igor Chestnov <inchestnov@gmail.com>
2023-11-29 22:19:12 +0300

    second (3)

---
+++ src/main/java/ru/gb/lesson2/Questions.java
@@ -0,0 +1,25 @@
+package ru.gb.lesson2;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Questions {
+
+  public static void main(String[] args) {
+    List<Integer> ints = List.of();
+
+    Optional<Integer> max = ints.stream()
+      .max(Integer::compare);
+    max.ifPresent(Questions::save);
+
+
+
+
+
+  }
+
+  private static void save(int value) {
+    //
+  }
+
+}
